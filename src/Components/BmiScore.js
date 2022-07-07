3ada60a1be6b348553bbd5b545992de3bc847ee7
Haroon Habeeb <haroon4040@gmail.com>
2022-07-07 12:41:35 +0530

    Final file

---
+++ src/Components/BmiScore.js
@@ -0,0 +1,42 @@
+function BmiScore({ bmiNo, bmiName, changeWeight }) {
+  return (
+    <div className="text-center shadow rounded p-4">
+      <div>Your BMI Score</div>
+      <div className="row justify-content-md-center">
+        {changeWeight.type === "positive" && (
+          <div className="p-3 my-2 alert fs-1 alert-danger col-sm-4">
+            {bmiNo}
+          </div>
+        )}
+        {changeWeight.type === "negative" && (
+          <div className="p-3 my-2 alert fs-1 alert-warning col-sm-4">
+            {bmiNo}
+          </div>
+        )}
+        {changeWeight.type === "normal" && (
+          <div className="p-3 my-2 alert fs-1 alert-success col-sm-4">
+            {bmiNo}
+          </div>
+        )}
+      </div>
+      <div className="fs-3 fw-bold text-primary">{bmiName}</div>
+      {changeWeight.type === "positive" && (
+        <div className="fs-4">
+          "You need to lose{" "}
+          <span className="fw-bold">{changeWeight.weight} kg"</span>{" "}
+        </div>
+      )}
+      {changeWeight.type === "negative" && (
+        <div className="fs-4">
+          "You need to gain{" "}
+          <span className="fw-bold">{changeWeight.weight} kg"</span>{" "}
+        </div>
+      )}
+      {changeWeight.type === "normal" && (
+        <div className="fs-4">"You weight is Normal, Good for you" </div>
+      )}
+    </div>
+  );
+}
+
+export default BmiScore;
